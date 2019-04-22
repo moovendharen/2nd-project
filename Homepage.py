17cd4dd9d57ff1ba9d211156517313793515063d
moovendharen <divyadivya8238@gmail.com>
2019-04-22 11:45:24 +0530

    commit by divya

--- Homepage.py
+++ Homepage.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def student():
+    divya
     return render_template("divya.html")
     #return render_template('student.html')
 if __name__=="__main__":
