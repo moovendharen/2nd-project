65bde3990d126e1b2b6d0c458bbf9522b99933b9
moovendharen <49509069+moovendharen@users.noreply.github.com>
2019-04-22 11:59:56 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def student():
-    divya
+    divya durga
     return render_template("divya.html")
     #return render_template('student.html')
 if __name__=="__main__":
